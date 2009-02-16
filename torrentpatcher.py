0cf81b9b05e15394e74488e78dc73dc5530b27ac
Andrey.Raygorodskiy <Andrey.Raygorodskiy@fb1afe42-f9bb-11dd-8c4e-4bbb75a408b7>
2009-02-16 11:16:10 +0000

    add draft version of GTK application

--- torrentpatcher.py
+++ torrentpatcher.py
@@ -1,31 +1,34 @@
-from time import sleep
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import urllib
-import subprocess
-import sys
+from configobj import ConfigObj
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
 __author__="araygorodskiy"
 __date__ ="$04.02.2009 17:56:17$"
 def download_tracker_ini():
-    file = urllib.urlopen("http://re-tracker.ru/trackerssimple.ini",proxies=None);
-    file2 = open("trackerssimple.ini","w")
-    file2.write(file.read());
-    try:
-        retcode = subprocess.call("iconv"+" -f UTF16LE -t UTF8 trackerssimple.ini -o trackerssimple-utf8.ini",shell=True);
-        if retcode < 0:
-            print >>sys.stderr, "Child was terminated by signal", -retcode
-        else:
-            print >>sys.stderr, "Child returned", retcode
-    except OSError, e:
-        print >>sys.stderr, "Execution failed:", e
+#    file = urllib.urlopen("c",proxies=None);
+#    file2 = open("trackerssimple.ini","w")
+#    file2.write(file.read());
+    config = ConfigObj('trackerssimple-utf8.ini');
+    section = config['Город'];
+    kolvo = section['Количество'];
+#    try:
+#        retcode = subprocess.call("iconv"+" -f UTF16LE -t UTF8 trackerssimple.ini -o trackerssimple-utf8.ini",shell=True);
+#        if retcode < 0:
+#            print >>sys.stderr, "Child was terminated by signal", -retcode
+#        else:
+#            print >>sys.stderr, "Child returned", retcode
+#    except OSError, e:
+#        print >>sys.stderr, "Execution failed:", e
 #    p = subprocess.Popen("iconv -f UTF16LE -t UTF8 trackerssimple.ini", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
 #    (child_stdin, child_stdout) = (p.stdin, p.stdout)
 #    i = os.system("iconv -f UTF16LE -t UTF8 trackerssimple.ini -o trackerssimple-utf8.ini");
 #    print i
-    sleep(10)
-    file3 = open("trackerssimple-utf8.ini","r");
-    print file3.read();
+#    sleep(10)
+#    file3 = open("trackerssimple-utf8.ini","r");
+#    print file3.read();
 #    print(child_stdout.read())
     pass;
 if __name__ == "__main__":
