22a807558bac5e082d3ff2a0bb8cce603542471d
Andrey.Raygorodskiy <Andrey.Raygorodskiy@fb1afe42-f9bb-11dd-8c4e-4bbb75a408b7>
2009-02-18 11:44:49 +0000

    add draft version of ini parser

---
+++ trunk/torrentpatcher_cli.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+from inifile import INIFile;
+__author__="araygorodskiy"
+__date__ ="$18.02.2009 14:36:25$"
+
+if __name__ == "__main__":
+    print "Hello World";
