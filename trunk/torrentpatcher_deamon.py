48c06f103c90f8e2a496366d85087255e77f99e2
Andrey.Raygorodskiy <Andrey.Raygorodskiy@fb1afe42-f9bb-11dd-8c4e-4bbb75a408b7>
2009-03-19 07:21:27 +0000

    bug fix

--- trunk/torrentpatcher_deamon.py
+++ trunk/torrentpatcher_deamon.py
@@ -1,22 +1,22 @@
-import ini
-#! /usr/bin/python
-from inifile import INIFile
-from time import sleep
-import bencode
+#!/usr/bin/env python
+
 import time
-import tp_config
-import datetime
+from time import sleep
+
+import bencode
+from inifile import INIFile
 import os
-__author__="araygorodskiy"
-__date__ ="$24.02.2009 14:52:22$"
+import tp_config
+__author__ = "araygorodskiy"
+__date__ = "$24.02.2009 14:52:22$"
 class Deamon(object):
     def __init__(self):
         self.config = tp_config.get_file();
-        self.from_dir = self.config.get('deamon','from_dir');
-        self.to_dir = self.config.get('deamon','to_dir');
-        self.city_number = int(self.config.get('config','city'));
-        self.prov_number = int(self.config.get('config','provider'));
-        self.last_update_time=0.0
+        self.from_dir = self.config.get('deamon', 'from_dir');
+        self.to_dir = self.config.get('deamon', 'to_dir');
+        self.city_number = int(self.config.get('config', 'city'));
+        self.prov_number = int(self.config.get('config', 'provider'));
+        self.last_update_time = 0.0
         self.ini = INIFile();
 
     def exec_transform(self):
@@ -26,14 +26,14 @@ class Deamon(object):
             name, ext = os.path.splitext(f)
             if ext == ".torrent":
                 args.append(os.path.join(self.from_dir, f))
+        urls = self.ini.get_urls_for_city_and_prov(self.city_number, self.prov_number);
         for torr in args:
             modif_time = os.stat(torr).st_mtime
             #print(torr)
             if modif_time > self.last_update_time:
                 #print "Accept %s" % torr
-                urls = self.ini.get_urls_for_city_and_prov(self.city_number,self.prov_number);
-                bencode.add_URLs_to_torrent(args,urls,self.to_dir);
-                os.utime(torr, (time.time(),time.time()))
+                bencode.add_URLs_to_torrent(args, urls, self.to_dir);
+                os.utime(torr, (time.time(), time.time()))
             #else:
                 #print "Decline %s" % torr
 
@@ -41,7 +41,7 @@ class Deamon(object):
         while True:
             #print "Update time = %s" % self.last_update_time
             self.exec_transform()
-            self.last_update_time=time.time();
+            self.last_update_time = time.time();
             sleep(1)
 
 
